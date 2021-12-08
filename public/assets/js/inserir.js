6c3d0de2a2f5439d22922cc9ca87b79b785bba97
Mikaéla <mikapetronilho@gmail.com>
2021-12-08 14:34:16 -0300

    inserir

--- public/assets/js/inserir.js
+++ public/assets/js/inserir.js
@@ -6,6 +6,7 @@ var i = 0
 var u = 0
 var k = 0
 var l = 0
+var j = 0
 function Enviar(){
     var pessoas = document.querySelector('#pess').value
     var quarto = document.querySelector('#qua').value
@@ -24,7 +25,7 @@ function Enviar(){
     elemento.setAttribute('id', i++)
     elemento.innerHTML = 'Data: '+datas+' Número de pessoas: '+pessoas+' Tipo de quarto: '+quarto
     elemento2.innerHTML = 'EXCLUIR'
-    elemento3.innerHTML = 'ALTERAR'
+    elemento3.innerHTML = 'SELECIONE PARAR ALTERAR'
     elemento2.addEventListener('click',Excluir)
     elemento3.addEventListener('click',Alterar)
     function Excluir(){
@@ -36,12 +37,28 @@ function Enviar(){
     }
         function Alterar(){
             if(k==i){
+                elemento3.innerHTML = 'ALTERAR'
+                j++//controlador do botao
+                var pessoas1 = document.querySelector('#pess')
+                var quarto1 = document.querySelector('#qua')
+                var datas1 = document.querySelector('#date')
+                elemento.style.color = 'blue'
+                pessoas1.style.borderColor = 'blue'
+                quarto1.style.borderColor = 'blue'
+                datas1.style.borderColor = 'blue'
+                if(j==2){
+                    elemento3.innerHTML = 'SELECIONE PARA ALTERAR'
+                    elemento.style.color = 'black'
+                pessoas1.style.borderColor = 'black'
+                quarto1.style.borderColor = 'black'
+                datas1.style.borderColor = 'black'
                 var pessoas1 = document.querySelector('#pess').value
                 var quarto1 = document.querySelector('#qua').value
                 var datas1 = document.querySelector('#date').value
+        
                 elemento.innerHTML = 'Data: '+datas1+' Número de pessoas: '+pessoas1+' Tipo de quarto: '+quarto1
-
-
+                j=0
+            }
             }
         }
     
